68666214bf6e2fbc4d60e056cd3917cf7f950f09
devchon2 <rchon@rchon.fr>
2025-02-28 12:32:14 +0100

    feat: mettre à jour la configuration et les dépendances pour le support de busboy

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
-  maxTokens: 2000000,
+  url: "https://docs.github.com/fr/copilot/",
+  match: "https://docs.github.com/fr/copilot/**",
+  maxPagesToCrawl: 500,
+  outputFileName: "copilot_doc_officielle.json",
+  maxTokens: 20000000,
 };
